9b7f94ef6da15dddf79bf9df26bc50bf0e16a317
Don Gagne <don@thegagnes.com>
2026-02-01 18:23:07 -0800

    Remove system update and python package installs

--- rpi_setup.sh
+++ rpi_setup.sh
@@ -3,13 +3,8 @@ set -e
 
 # wget https://raw.githubusercontent.com/DonLakeFlyer/STEInfrasound/main/rpi_setup.sh
 
-echo "*** Update system"
-sudo apt update
-sudo apt full-upgrade -y
-
 echo "*** Install tools"
 sudo apt install build-essential git cmake -y
-pip install -r requirements.txt
 git config --global pull.rebase false
 
 echo "*** Create repos directory"
