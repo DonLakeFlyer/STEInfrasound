3db84abdbeaa4dbf4a50a64e2353d1a93e897a69
Don Gagne <don@thegagnes.com>
2026-02-04 12:24:02 -0800

    Don't wait 10 seconds

--- start_infrasound.sh
+++ start_infrasound.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for 10 seconds to ensure all services are up and usb devices are ready
-echo "Sleeping for 10 seconds to allow services to start..."
-sleep 10
-
 # Activate your Python environment
 source /home/pi/repos/STEInfrasound/venv/bin/activate
 
